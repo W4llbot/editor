6ecdefe76b274e58927bc1db0e0988d26d09fc9d
W4llbot <cahchenanhong@gmail.com>
2026-07-18 14:19:08 +0800

    added more flags

--- kilo.c
+++ kilo.c
@@ -1,3 +1,5 @@
+#include <ctype.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <termios.h>
@@ -15,7 +17,23 @@ void enableRawMode()
     atexit(disableRawMode);
 
     struct termios raw = orig_termios;
-    raw.c_lflag &= ~(ECHO | ICANON);
+
+    /**
+     * IXON: turns off software flow control (Ctrl-S and Ctrl-Q)
+     * ICRNL: turns off automatically translating CR to newline (fixes Ctrl-M)
+     * Optional (legacy flags): BRKINT, INPCK, ISTRIP, CS8
+    */
+    raw.c_iflag &= ~(BRKINT | INPCK | ISTRIP | ICRNL | IXON);
+    raw.c_cflag |= (CS8);
+    // OPOST: turns off all output processing (EG: translating \n to \r\n)
+    raw.c_oflag &= ~(OPOST);
+    /**
+     * ECHO: turns off echoing characters typed
+     * ICANON: reads every byte instead of only on newline
+     * ISIG: turns of Ctrl-Z and Ctrl-C
+     * IEXTEN: turn off Ctrl-V for verbatim
+    */
+    raw.c_lflag &= ~(ECHO | ICANON | ISIG | IEXTEN);
 
     tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
@@ -23,7 +41,15 @@ void enableRawMode()
 int main(void)
 {
     enableRawMode();
+
     char c;
-    while (read(STDIN_FILENO, &c, sizeof(char)) == 1 && c != 'q');
+    while (read(STDIN_FILENO, &c, sizeof(char)) == 1 && c != 'q') {
+        if (iscntrl(c)) {
+            printf("%d\r\n", c);
+        } else {
+            printf("%d ('%c')\r\n", c, c);
+        }
+    }
+
     return 0;
 }
